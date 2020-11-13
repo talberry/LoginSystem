3f32112fa5ef9a2ee1b5761fe233b4fc4fe576f4
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2020-11-12 19:18:03 -0800

    Added removeUser Rout

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+//server.js
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
 
-const __dirname = path.resolve(path.dirname('')); 
+const __dirname = path.resolve(path.dirname(''));
+
+const port = process.env.PORT||3000;
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.use('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client', 'main', 'index.html'));
 });
 
-app.use('/signUp/:username/:password', (req, res)=>{
+app.get('/signUp/:username/:password', (req, res)=>{
     const info = JSON.parse(fs.readFileSync('./info.json'));
     const username = req.params.username;
     const password = req.params.password;
+    const id = info.length;
 
     info.push({
+        id: id,
         username: username,
         password: password
     });
@@ -26,7 +30,22 @@ app.use('/signUp/:username/:password', (req, res)=>{
     res.send(info);
 });
 
-app.listen(port, ()=>{
-    console.log(`App started on port: ${port}`);
+app.get('/removeUser/:id', (req, res)=>{
+    const info = JSON.parse(fs.readFileSync('./info.json'));
+    const id = req.params.id;
+    const removedUser = info[id]
+
+    fs.writeFileSync('./info.json', JSON.stringify(info.slice(id, id + 1)));
+
+    res.send(removedUser);
 });
 
+app.get('/users', (req, res)=>{
+    const info = JSON.parse(fs.readFileSync('./info.json'));
+
+    res.send(info);
+});
+
+app.listen(port, ()=>{
+    console.log('server running on port:'+port);
+});
